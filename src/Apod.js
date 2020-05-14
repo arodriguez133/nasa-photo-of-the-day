40a2315b99633d1f4bee25e8d7bcb3749e489679
Adan Rodriguez <adanrodriguez@Kourtneys-MBP.hitronhub.home>
2020-05-13 22:24:30 -0500

    added styles using styled components. Moving on now to figuring out how to add gsap

--- src/Apod.js
+++ src/Apod.js
@@ -15,12 +15,45 @@ function Apod() {
 
     console.log(apod)
 
+    const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 20px 2fr;
+    grid-template-rows: .5fr .5fr 1fr;
+    `
+
+    const Title = styled.h1 `
+    font-family: Helvetica Neue, Arial;
+    font-weight: 700;
+    font-size: 64px;
+    text-align: center;
+    letter-spacing: 1px;
+    margin-left: 17px;
+    `
+
+    const Content = styled.div`
+        margin: 0;
+        grid-area: 2 / 1 / 3 /3;
+        place-self: center;
+    `
+    const Explanation = styled.p `
+        text-align: center;
+        font-size: 18px;
+    `
+
+    const Image = styled.img `
+      width: 100%;
+      grid-area:   2 / 4 / 3 / 5;
+      place-self: stretch;
+    `
+
     return (
-        <div className="App">
-            <h1>{apod.copyright}</h1>
-            <p>{apod.explanation}</p>
-            <img src = {apod.url}></img>
-        </div>
+        <GridContainer>
+            <Content>
+            <Title>{apod.title}</Title>
+            <Explanation>{apod.explanation}</Explanation>
+            </Content>
+            <Image src = {apod.url}></Image>
+        </GridContainer> 
     );
 }
 
